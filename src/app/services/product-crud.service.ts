6ee68199de88c26a3a846b203abfca06d3b557fb
Juan Cortes <alias99002@outlook.com>
2023-04-19 10:30:41 -0500

    decorators

--- src/app/services/product-crud.service.ts
+++ src/app/services/product-crud.service.ts
@@ -1,6 +1,7 @@
 import { UpdateProductDto } from "../dtos/product.dto";
 import { Product } from "../models/product.model";
 import { ProductHttpService } from "./product-htp2.service";
+import { validate } from "class-validator";
 
 export class ProductCRUDService {
   private url = 'https://api.escuela.co/api/v1/products';
@@ -8,6 +9,7 @@ export class ProductCRUDService {
 
   async update(id: Product['id'], dto: UpdateProductDto) {
     //code
+    // validate(dto);
     // inheritance with generic types
     return this.http.anotherRequest()
   }
